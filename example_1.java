eca1e923774c6e94d5e6dd194a0f2be8f7f4394f
Danii9790 <daniyalashraf9790@gmail.com>
2025-09-05 18:11:29 +0500

    Update

---
+++ example_1.java
@@ -0,0 +1,20 @@
+public class example_1 {
+    public static void main(String[] args){
+        // Student data
+        String studentName = "M.Daniyal";
+        int studentAge = 20;
+        int studentID = 34;
+        float studentFee = 28.00f;
+        char StudentGrade = 'A';
+        boolean isGoodStudent = true;
+
+        // Print variables
+        System.out.println("Student Name : "+" " + studentName);
+        System.out.println("Student Age : "+" " + studentAge);
+        System.out.println("Student ID : "+" " + studentID);
+        System.out.println("Student Fee : "+" " + studentFee);
+        System.out.println("Student Grade : "+" " + StudentGrade);
+        System.out.println("Student IsGood Student : "+" " + isGoodStudent);
+        
+    }
+}
---
+++ example_2.java
@@ -0,0 +1,15 @@
+public class example_2 {
+    public static void main(String[] args){
+        // Create integer variable
+        int length = 4 ;
+        int width = 5 ;
+        int area;
+
+        area = length * width;
+
+        // Print Variables
+        System.out.println("Length : "+" " + length);
+        System.out.println("Width : "+" " + width);
+        System.out.println("Area : "+" " + area);
+    }
+}
