ee09f3f98ff39e31cfdc0bdbe10c1610c746b916
Danii9790 <daniyalashraf9790@gmail.com>
2025-09-07 10:37:38 +0500

    Update

---
+++ Data_types.java
@@ -0,0 +1,13 @@
+public class Data_types{
+    public static void main(String[] args){
+        int item = 50;
+        float costperItem = 10.5f;
+        float totalCost = item * costperItem;
+        char currency = '$';
+
+        System.out.println("Number of items : " + " " + item);
+        System.out.println("Cost per Item : " + " " + costperItem);
+        System.out.println("Total Cost : " + " " + totalCost + currency);
+
+    }
+}
---
+++ var_type.java
@@ -0,0 +1,11 @@
+public class var_type {
+    public static void main(String[] args){
+        var myName = "M.Daniyal";
+        var age = 21;
+
+        System.out.println("My Name is : " + " " + myName);
+        System.out.println("My Age is : " + " " + age);
+        System.out.println("Data Type of Age is  : " + " " +((Object)age).getClass().getSimpleName());
+
+    }
+}
